44eff5a4b432b73654d5047bed9482bbbf19a529
Ethan Mines <elmines325@gmail.com>
2018-10-22 16:26:09 -0500

    Plot packets together on one graph

--- plot.py
+++ plot.py
@@ -7,12 +7,22 @@ fileName = sys.argv[1]
 with open(fileName, "r", encoding="utf-8") as notes:
     eegGood = json.load(notes)
 
+numPackets = len(eegGood)
+samplesPerPacket = len(eegGood[0]["data"][0])
+samplingRate = 256 #Hz
+assert samplesPerPacket == samplingRate
+timestamps = []
+for i in range(numPackets):
+    timestamps.extend([i + j/samplingRate for j in range(samplingRate) ])
+
 channels = eegGood[0]["info"]["channelNames"]
-numSamples = len(eegGood[0]["data"][0])
-timestamps = [i for i in range(numSamples)]
-for packet in eegGood:
-    for i, channel in enumerate(channels):
-        samples = packet["data"][i]
-        plt.plot(timestamps, samples, label=channel)
-        plt.legend()
-    plt.show()
+for i, channel in enumerate(channels):
+     samples = []
+     for packet in eegGood:
+         samples.extend(packet["data"][i])
+     plt.plot(timestamps, samples, label=channel)
+plt.xlabel("Time (s)")
+plt.ylabel(u"Voltage (\u03bcV)")
+plt.legend()
+plt.show()
+
