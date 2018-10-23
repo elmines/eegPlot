9a36529ee7fd3876981791d998510791a88ff21d
Ethan Mines <elmines325@gmail.com>
2018-10-23 10:05:16 -0500

    Plot individual packets or all the packets together

--- plot.py
+++ plot.py
@@ -3,26 +3,38 @@ import matplotlib
 import matplotlib.pyplot as plt
 import json
 
+def display():
+	plt.xlabel("Time (s)")
+	plt.ylabel(u"Voltage (\u03bcV)")
+	plt.legend()
+	plt.show()
+
 fileName = sys.argv[1]
 with open(fileName, "r", encoding="utf-8") as notes:
-    eegGood = json.load(notes)
+	eegGood = json.load(notes)
+onePlot = True
+if len(sys.argv) > 2:
+	onePlot = False	
 
 numPackets = len(eegGood)
 samplesPerPacket = len(eegGood[0]["data"][0])
 samplingRate = 256 #Hz
 assert samplesPerPacket == samplingRate
-timestamps = []
-for i in range(numPackets):
-    timestamps.extend([i + j/samplingRate for j in range(samplingRate) ])
 
 channels = eegGood[0]["info"]["channelNames"]
-for i, channel in enumerate(channels):
-     samples = []
-     for packet in eegGood:
-         samples.extend(packet["data"][i])
-     plt.plot(timestamps, samples, label=channel)
-plt.xlabel("Time (s)")
-plt.ylabel(u"Voltage (\u03bcV)")
-plt.legend()
-plt.show()
+if onePlot:
+	timestamps = []
+	for i in range(numPackets):
+		timestamps.extend([i + j/samplingRate for j in range(samplingRate) ])
+	for i, channel in enumerate(channels):
+		samples = []
+		for packet in eegGood:
+			samples.extend(packet["data"][i])
+		plt.plot(timestamps, samples, label=channel)
+else:
+	timestamps = [j / samplingRate for j in range(samplingRate)]
+	for packet in eegGood:
+		for i, channel in enumerate(channels):
+			plt.plot(timestamps, packet["data"][i], label=channel)
+		display()
 
